cc1d0f3e9816af264a91c1df2f14d3083bc8e01c
Paurus3 <pau.gamez.pallares@enti.cat>
2022-02-02 19:44:04 +0100

    Lego

--- app/src/main/java/com/example/appclasse/RegisterActivity.kt
+++ app/src/main/java/com/example/appclasse/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.appclasse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
@@ -7,8 +8,11 @@ import android.view.View
 import android.widget.AbsSeekBar
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
+import com.example.appclasse.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import java.lang.ref.PhantomReference
@@ -45,10 +49,10 @@ class RegisterActivity : AppCompatActivity() {
 
     fun register(view:View)
     {
-
-
+        createAccount()
     }
 
+    //Registramos una nueva cueta
     private fun createAccount()
     {
         val name:String = txtName.text.toString()
@@ -60,8 +64,49 @@ class RegisterActivity : AppCompatActivity() {
         {
             progressBar.visibility = View.VISIBLE
 
-            auth.c
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this)
+            {
+                task->
 
+                if (task.isComplete)
+                {
+                    val user:FirebaseUser?=auth.currentUser
+                    verifyEmail(user)
+
+                    //Enviamos datos a la BD
+
+                    val userBD=dbReference.child(user?.uid)
+
+                    userBD.child("Name").setValue(name)
+                    userBD.child("LastName").setValue(lastName)
+                    action()
+                }
+            }
         }
     }
+
+    private fun action()
+    {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+
+    //Verificacion de correo
+    private fun verifyEmail(user: FirebaseUser?)
+    {
+        user?.sendEmailVerification()?.addOnCompleteListener(this)
+        {
+            task ->
+
+            if (task.isComplete)
+            {
+                Toast.makeText(this, "Email enviado", Toast.LENGTH_LONG).show()
+            }else
+            {
+                Toast.makeText(this, "Error al enviar", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
 }
