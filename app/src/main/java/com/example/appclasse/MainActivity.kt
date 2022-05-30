8ea554484b61020619dc7540e268b232c615c4fc
Paurus3 <pau.gamez.pallares@enti.cat>
2022-05-30 18:36:06 +0200

    git

--- app/src/main/java/com/example/appclasse/MainActivity.kt
+++ app/src/main/java/com/example/appclasse/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.appclasse
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.text.format.DateFormat
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import com.example.appclasse.databinding.ActivityMainBinding
@@ -37,6 +40,8 @@ public class MainActivity : AppCompatActivity()
         binding.MainButton.setOnClickListener{
             initialCountDown =  binding.texInputEditTextNumber.text.toString().toLong() *60000
             binding.texInputEditTextNumber.visibility = View.INVISIBLE
+            val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
             startStop()
         }
 
@@ -224,7 +229,10 @@ public class MainActivity : AppCompatActivity()
         countDownTimer.cancel()
         timeRunning = false
         binding.MainButton.text = "Start"
-        binding.Timer.text = "00:00"
+        val rep = Calendar.getInstance()
+        rep.set(0,0,0,0,0,0)
+        rep.set(Calendar.MILLISECOND, initialCountDown.toInt())
+        binding.Timer.text = DateFormat.format("mm:ss", rep.time)
     }
 
     fun setTimer()
@@ -232,12 +240,18 @@ public class MainActivity : AppCompatActivity()
 
     }
 
+    private fun View.hideKeyboard() {
+
+    }
     override fun onPause() {
         super.onPause()
         countDownTimer.cancel()
         timeRunning = false
         binding.MainButton.text = "Start"
-        binding.Timer.text = "00:00"
+        val rep = Calendar.getInstance()
+        rep.set(0,0,0,0,0,0)
+        rep.set(Calendar.MILLISECOND, initialCountDown.toInt())
+        binding.Timer.text = DateFormat.format("mm:ss", rep.time)
     }
 
 
